9f2db0bc33d0b45ba3d4feb4e458178d182a0c1e
Ostap Shopyak <90937268+ssshopyak@users.noreply.github.com>
2023-04-03 17:44:47 +0300

    Updates

---
+++ precache-manifest.20147fde68e532b7ea23f7952ad7f678.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8842bedbed7721f114244e5e8508d0f7",
+    "url": "/armstrong/index.html"
+  },
+  {
+    "revision": "b433549551645186f825",
+    "url": "/armstrong/static/css/main.267be62e.chunk.css"
+  },
+  {
+    "revision": "457bb27a8f675660811b",
+    "url": "/armstrong/static/js/2.8333fc3c.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/armstrong/static/js/2.8333fc3c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b433549551645186f825",
+    "url": "/armstrong/static/js/main.07685319.chunk.js"
+  },
+  {
+    "revision": "d943b96d7c77e9f86339",
+    "url": "/armstrong/static/js/runtime-main.fcbeadfb.js"
+  },
+  {
+    "revision": "cdfe38b9cb6286e215d032e87a2cd4ad",
+    "url": "/armstrong/static/media/Logo.cdfe38b9.png"
+  },
+  {
+    "revision": "ac219878fc12c1ff89804879481b1acb",
+    "url": "/armstrong/static/media/aboutus.ac219878.jpg"
+  },
+  {
+    "revision": "207837350e0b86b46c551f17d0b027e8",
+    "url": "/armstrong/static/media/email.20783735.png"
+  },
+  {
+    "revision": "3ce6ca34106506d83976111d786801f1",
+    "url": "/armstrong/static/media/guarantee.3ce6ca34.png"
+  },
+  {
+    "revision": "d2631c1815d4d979da9175056609f89c",
+    "url": "/armstrong/static/media/location.d2631c18.png"
+  },
+  {
+    "revision": "994bbde2ce0d05d8cdb0c9627095f118",
+    "url": "/armstrong/static/media/support.994bbde2.png"
+  },
+  {
+    "revision": "5ff8a1ee14fd7f4248c91afc502ad0ab",
+    "url": "/armstrong/static/media/tracking.5ff8a1ee.png"
+  }
+]);
